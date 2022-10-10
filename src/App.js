242934baab93687ae230bde9fcf94a5534440d23
Alameen8739 <allialameen8739@gmail.com>
2022-10-10 21:13:01 +0100

    add page layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import mobile from "./images/image-hero-mobile.png";
+import desktop from "./images/image-hero-desktop.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="grid grid-cols-1 gap-10" >
+        <article>
+          <h1>Make remote work</h1>
+          <p>
+            Get your team in sync, no matter your location. Streamline processes,
+            create team rituals, and watch productivity soar.
+          </p>
+          <button>
+            Learn more
+          </button>
+        </article>
+
+        <article>
+          <picture>
+            <source media="(min-width: 768px)" scrSet={desktop} />
+            <img src={mobile} alt="" />
+          </picture>
+        </article>
+      </section>
+    </>
   );
 }
 
