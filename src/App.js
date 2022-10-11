dd4c554e3999795e2354d09ca0d57f7a1a59b9c3
Alameen8739 <allialameen8739@gmail.com>
2022-10-11 14:53:01 +0100

    add navbar style

--- src/App.js
+++ src/App.js
@@ -4,10 +4,37 @@ import audiophile from "./images/client-audiophile.svg";
 import databiz from "./images/client-databiz.svg";
 import maker from "./images/client-maker.svg";
 import meet from "./images/client-meet.svg";
+import logo from "./images/logo.svg";
+import chevronDown from "./images/icon-arrow-down.svg"
+import menuOpen from "./images/icon-menu.svg"
 
 function App() {
   return (
     <>
+
+      <header className="p-5 flex items-center justify-between">
+        <div>
+          <img src={logo} alt="" />
+          <nav className="navbar">
+            <div><button>Features <img src={chevronDown} /></button></div>
+            <div><button>Company <img src={chevronDown} /></button></div>
+            <div><button>Careers</button></div>
+            <div><button>About</button></div>
+          </nav>
+        </div>
+
+        <div>
+          <button>
+            <img src={menuOpen} alt="" />
+          </button>
+        </div>
+
+        <div className="hidden lg:block">
+          <button>Login</button>
+          <button>Sign Up</button>
+        </div>
+      </header>
+
       <section className="flex flex-col-reverse py-20 md:grid md:gap-10 md:grid-cols-2 md:place-items-center max-w-4xl lg:max-w-5xl md:mx-auto" >
         <article className="text-center md:text-left px-5 xl:px-0 mt-10 md:mt-0">
           <h1 className="font-bold text-4xl lg:text-6xl xl:text-7xl mb-5">Make remote work</h1>
@@ -26,7 +53,7 @@ function App() {
             <li><img src={meet} className="w-16 md:w-24" /></li>
           </ul>
 
-        </article>  
+        </article>
 
         <article>
           <picture>
